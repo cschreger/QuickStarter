1a3b66c708a2d383b77b1cc774b25dec72b9043e
Cade Schreger <cschreger@gmail.com>
2020-07-27 17:18:33 -0400

    Prepare for initial Heroku push

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,14 +6,11 @@ class Api::UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        debugger
 
         if @user.save
-            debugger
             login!(@user)
             render json: ["Success"]
         else
-            debugger
             render json: @user.errors.full_messages, status: 422
         end
     end
