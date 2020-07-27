f2b49e721a3d509164c1aa47694e8624cb9e93c2
Cade Schreger <cschreger@gmail.com>
2020-07-27 14:05:55 -0400

    Sessions & Users controllers

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,12 +1,29 @@
 class SessionsController < ApplicationController
 
     def create
+        @user = User.find_by_credentials(
+            params[:user][:name],
+            params[:user][:password]
+        )
 
+        if @user
+            login(@user)
+            render "/"
+        else
+            render json: ["Invalid name/password combination"], status: 401
+        end
     end
 
 
     def destroy
         @user = current_user
+
+        if @user
+            logout!
+            render json: {}
+        else
+            render json: ["Nobody is signed in!"], status: 404
+        end
     end
     
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
-    
-    def create
 
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login!(@user)
+            render `api/users/#{@user.id}`
+        else
+            render json: ["Invalid!"], status: 422
+        end
     end
     
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password)
+    end
 end
