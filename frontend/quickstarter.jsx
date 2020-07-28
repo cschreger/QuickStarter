cc579ecb4de744ff908385b8ad566c07e5a199ed
Cade Schreger <cschreger@gmail.com>
2020-07-27 20:59:04 -0400

    store tested and Router add

--- frontend/quickstarter.jsx
+++ frontend/quickstarter.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {signup, login, logout} from './util/session_api_util'
+import configureStore from './store/store'
 
 document.addEventListener("DOMContentLoaded", () => {
+    const store = configureStore();
+    debugger
+    window.getState = store.getState;
+    debugger
+    window.dispatch = store.dispatch;
     const root = document.getElementById("root")
     ReactDOM.render(<h1>Welcome to Quickstarter!</h1>, root)
 });
+// const store = configureStore();
